08f5629bbebe3a5c282d40e7eafe63164b10add0
Delawit12 <131721430+Delawit12@users.noreply.github.com>
2024-05-31 14:31:33 +0300

    project update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,16 +19,16 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={appleBootstrap}
               isBlog={false}
               title="Apple-clone"
               description=""
               // ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              // demoLink="https://chatify-49.web.app/"
             />
-          </Col>
+          </Col> */}
 
           <Col md={4} className="project-card">
             <ProjectCard
@@ -36,8 +36,8 @@ function Projects() {
               isBlog={false}
               title="Evangadi-forum"
               description=""
-              // ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              // demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="https://github.com/Delawit12/evangadi-forum-front"
+              demoLink="https://evangadi-forum-page.netlify.app"
             />
           </Col>
 
@@ -47,8 +47,8 @@ function Projects() {
               isBlog={false}
               title="amazon-clone"
               description=""
-              // ghLink="https://github.com/soumyajit4419/Editor.io"
-              // demoLink="https://editor.soumya-jit.tech/"
+              ghLink="https://github.com/Delawit12/Amazon-clone-frontend"
+              demoLink="https://amazon-frontend-clone-new.netlify.app"
             />
           </Col>
 
@@ -58,8 +58,8 @@ function Projects() {
               isBlog={false}
               title="netflix-clone"
               description=""
-              // ghLink="https://github.com/soumyajit4419/Plant_AI"
-              // demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="https://github.com/Delawit12/Netflix-front-Clone"
+              demoLink="neflix-page-clone.netlify.app"
             />
           </Col>
         </Row>
